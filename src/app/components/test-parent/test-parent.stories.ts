3db3755be82f97276fa9d8a39bf7109677901aa9
-- <-->
2021-07-01 13:33:57 -0400

    add - storybook

---
+++ src/app/components/test-parent/test-parent.stories.ts
@@ -0,0 +1,29 @@
+import { moduleMetadata } from '@storybook/angular';
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import { TestParentComponent } from './test-parent.component';
+
+// 👇 The default export metadata controls how Storybook lists your stories and provides information used by addons.
+export default {
+  title: 'Test Components',
+  component: TestParentComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [TestParentComponent],
+      imports: [],
+    }),
+  ],
+} as Meta;
+
+// 👇 We create a "template" of how args map to rendering
+const template: Story<TestParentComponent> = (args: TestParentComponent) => ({
+  template: `
+   <app-test-parent></app-test-parent>
+`,
+  props: {
+    ...args,
+  },
+});
+
+// 👇 Each story can then reuse that template
+export const generalUsage: Story<TestParentComponent> = template.bind({});
+generalUsage.args = {};
